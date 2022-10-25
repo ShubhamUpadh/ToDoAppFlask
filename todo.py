93f43bd5959dc6798e1afa10d207b7474aecfff6
ShubhamUpad <01shubhamupadhyay@gmail.com>
2022-10-25 18:51:04 +0530

    First commit

---
+++ todo.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/about')
+def about():
+    return "About Something"
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
